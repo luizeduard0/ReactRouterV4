ab41594ec4804508d0a2f1976614692d6e3a0efe
Luiz Eduardo R Figueiredo Paglioni <luizeduard0@iMac-de-Luiz.local>
2017-09-16 00:14:29 -0300

    Redirect component

--- src/Router.js
+++ src/Router.js
@@ -118,4 +118,25 @@ class Link extends Component {
   }
 }
 
+class Redirect extends Component {
+  static defaultProps = {
+    push: false
+  }
+
+  static propTypes = {
+    to: PropTypes.string.isRequired,
+    push: PropTypes.bool.isRequired
+  }
+
+  componentDidMount() {
+    const { to, push } = this.props
+
+    push ? historyPush(to) : historyReplace(to)
+  }
+
+  render() {
+    return null
+  }
+}
+
 export {Route, Link}
